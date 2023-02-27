36fa38026f989f8ed3aa1d3ea7b2c89b5397131a
wanglei <806416060@qq.com>
2023-02-27 17:03:40 +0800

    完善动态导入功能

--- demo2/src/main.js
+++ demo2/src/main.js
@@ -2,8 +2,8 @@
  * @Author       : wanglei
  * @Date         : 2023-02-27 14:18:12
  * @LastEditors  : wanglei
- * @LastEditTime : 2023-02-27 14:18:25
- * @FilePath     : /wepback-code-split/demo1/src/main.js
+ * @LastEditTime : 2023-02-27 16:57:11
+ * @FilePath     : /wepback-code-split/demo2/src/main.js
  * @description  : 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
@@ -11,3 +11,12 @@ import {sum} from './math'
 
 console.log(sum(4,5,6))
 console.log('hello main')
+
+document.getElementById('btn').onclick = function(){
+  // import 动态导入，会将动态导入的文件代码分割(拆分成单独模块),在需要使用的时候自动加载
+  import('./count').then((res) => {
+    console.log('模块加载成功', res.default(3,4))
+  }).catch((err) => {
+    console.log('模块加载失败', err)
+  })
+}
--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -2,7 +2,7 @@
  * @Author       : wanglei
  * @Date         : 2023-02-27 14:16:26
  * @LastEditors  : wanglei
- * @LastEditTime : 2023-02-27 14:55:17
+ * @LastEditTime : 2023-02-27 15:15:45
  * @FilePath     : /wepback-code-split/demo2/webpack.config.js
  * @description  : 多入口配置,提取公共方法为单独文件
  */
@@ -17,6 +17,10 @@ module.exports = {
     main: './src/main.js'
   },
   output: {
+    // [name]是webpack命名规则，使用chunk的name作为输出的文件名。
+    // 什么是chunk？打包的资源就是chunk，输出出去叫bundle。
+    // chunk的name是啥呢？ 比如： entry中xxx: "./src/xxx.js", name就是xxx。注意是前面的xxx，和文件名无关。
+    // 为什么需要这样命名呢？如果还是之前写法main.js，那么打包生成两个js文件都会叫做main.js会发生覆盖。(实际上会直接报错的)
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js' // webpack命名方式，[name]以文件名自己的名字去命名
   },
@@ -30,8 +34,33 @@ module.exports = {
     // 代码分割配置
     splitChunks: {
       chunks: 'all', // 对所有模板都进行分割
+      // 以下是默认值
+      // minSize: 20000, // 分割代码最小的大小
+      // minRemainingSize: 0, // 类似于minSize，最后确保提取的文件大小不能为0
+      // minChunks: 1, // 至少被引用的次数，满足条件才会代码分割
+      // maxAsyncRequests: 30, // 按需加载时并行加载的文件的最大数量
+      // maxInitialRequests: 30, // 入口js文件最大并行请求数量
+      // enforceSizeThreshold: 50000, // 超过50kb一定会单独打包（此时会忽略minRemainingSize、maxAsyncRequests、maxInitialRequests）
+      // cacheGroups: { // 组，哪些模块要打包到一个组
+      //   defaultVendors: { // 组名
+      //     test: /[\\/]node_modules[\\/]/, // 需要打包到一起的模块
+      //     priority: -10, // 权重（越大越高）
+      //     reuseExistingChunk: true, // 如果当前 chunk 包含已从主 bundle 中拆分出的模块，则它将被重用，而不是生成新的模块
+      //   },
+      //   default: { // 其他没有写的配置会使用上面的默认值
+      //     minChunks: 2, // 这里的minChunks权重更大
+      //     priority: -20,
+      //     reuseExistingChunk: true,
+      //   },
+      // },
       // 修改配置
       cacheGroups: {
+        // 组，哪些模块要打包到一个组
+        // defaultVendors: { // 组名
+        //   test: /[\\/]node_modules[\\/]/, // 需要打包到一起的模块
+        //   priority: -10, // 权重（越大越高）
+        //   reuseExistingChunk: true, // 如果当前 chunk 包含已从主 bundle 中拆分出的模块，则它将被重用，而不是生成新的模块
+        // },
         // 其他没有写的配置，会使用上面的默认配置
         default: {
           minSize: 0, // 我们定义的文件体积太大了，要走默认配置的话，不会被打包，所以要更改打包的最小文件体积
